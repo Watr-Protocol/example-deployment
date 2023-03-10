a460fbd305f0551976a5c5640e6589147a585f1f
TriplEight <denis.pisarev@parity.io>
2023-03-10 17:40:31 +0100

    git cleanup

--- terraform/modules/cloudwatch/vars.tf
+++
@@ -1,4 +0,0 @@
-variable "cloudwatch" {
-  description = "Configuration of the cloudwatch nodes"
-  type        = map(object({ ami : string, instance_type : string, subnet : string, availability_zone : string, volume_size_gb : number }))
-}
